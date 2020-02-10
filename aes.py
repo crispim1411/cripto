53e6013c32065d40aa731224d56f9e0cae7e68eb
Rodrigo Crispim <crispim1411@gmail.com>
2020-02-10 23:49:18 +0000

    salt aleatório junto do texto encriptado
    gera salt aleatório e adiciona ao texto encriptado

--- aes.py
+++ aes.py
@@ -1,35 +1,41 @@
 import base64
 import hashlib
 import os
-#from Crypto import Random
 from Crypto.Cipher import AES
 
 block_size = 16
 pad = lambda s: s + (block_size - len(s) % block_size) * chr(block_size - len(s) % block_size)
 unpad = lambda s: s[0:-ord(s[-1:])]
-#iv = os.urandom(AES.block_size)  # Random IV
-#iv = "iv testando aaaa".encode()
+
+SALT_SIZE = 16
 
 
 def get_private_key(secret_key, salt):
-    return hashlib.pbkdf2_hmac('SHA256', secret_key.encode(), salt.encode(), 65536, 32)
+    return hashlib.pbkdf2_hmac('SHA256', secret_key.encode(), salt, 65536)
 
 
-def encrypt(message, secret_key, salt):
+def encrypt(message, secret_key):
+    salt = os.urandom(16)
+    iv = os.urandom(16)
+
     private_key = get_private_key(secret_key, salt)
     message = pad(message)
-    iv = "iv testando aaaa".encode()
+
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    cipher_bytes = base64.b64encode(iv + cipher.encrypt(message))
+    cipher_bytes = base64.b64encode(salt + iv + cipher.encrypt(message))
     return bytes.decode(cipher_bytes)
 
 
-def decrypt(encoded, secret_key, salt):
-    private_key = get_private_key(secret_key, salt)
+def decrypt(encoded, secret_key):
     cipher_text = base64.b64decode(encoded)
-    iv = cipher_text[:AES.block_size]
+    salt = cipher_text[:SALT_SIZE]
+    iv = cipher_text[SALT_SIZE:32]
+    content = cipher_text[32:]
+
+    private_key = get_private_key(secret_key, salt)
+
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    plain_bytes = unpad(cipher.decrypt(cipher_text[block_size:]));
+    plain_bytes = unpad(cipher.decrypt(content))
     return bytes.decode(plain_bytes)
 
 
@@ -49,20 +55,19 @@ def save_content(ciphertext):
 
 plain_text = "Mensagem criptografada no Python"
 secret_key = "yourSecretKey"
-salt = "anySaltYouCanUseOfOn"
 
 os.chdir("../AES_python_java/AES_python_java")
 
 # Python -> Java
-cipher_python = encrypt(plain_text, secret_key, salt)
+cipher_python = encrypt(plain_text, secret_key)
 save_content(cipher_python)
-decrypted_python = decrypt(cipher_python, secret_key, salt)
+decrypted_python = decrypt(cipher_python, secret_key)
 print(f"Cipher by Python: {cipher_python}")
 print(f"Decrypted: {decrypted_python}\n")
 
 # Java -> Python
 cipher_java = read_content()
-decrypted_java = decrypt(cipher_java, secret_key, salt)
+decrypted_java = decrypt(cipher_java, secret_key)
 print(f"Cipher by Java: {cipher_java}")
 print(f"Decrypted: {decrypted_java}")
 
